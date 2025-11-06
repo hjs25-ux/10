2ff888740265a2f9ac29c82d5f1ecfcd6f8812db
hjs25-ux <hjs25@sookmyung.ac.kr>
2025-11-06 16:25:15 +0900

    06

--- main.c
+++ main.c
@@ -1,25 +1,34 @@
 #include <stdio.h>
 #include <string.h>
 
-void main(void)
+int main(int argc, char *argv[])
 {
-  char input[100];
-  FILE*fp;
-  int i;
-  
-  fp = fopen("sample.txt","w");
-  
-  for(i=0;i<3;i++)
-  {
-  //fprintf
-  printf("input a word:");
-  scanf("%s", input);
-  fprintf(fp,"%s\n",input);
-}
-  
-  //fclose
-  fclose(fp);
+    char input[100];
+    FILE* fp;
+    char c;
        
+    fp=fopen("sample.txt","r");
+    if (fp == NULL)
+    {
+           printf("Failed to open file!\n");
+           return -1;
+    }
+    
+    while( (c=fgetc(fp)) != EOF)
+    {
+           putchar(c);
+    }
+    
+    
+    while ( fgets(input,100,fp) > 0)
+    {
+          printf("%s",input);
+    }
+    
+    
+    fclose(fp);
+    
+    
   system("PAUSE");	
   return 0;
 }

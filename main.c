929efc20a621344c5a54375ffb7083524984a9a8
hjs25-ux <hjs25@sookmyung.ac.kr>
2025-11-06 15:43:34 +0900

    05

--- main.c
+++ main.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 
-void main(void){
-          char str[30] = "happy C programming.";
-          
-          printf("문자열\"%s\"의 길이:%i",str,strlen(str)); 
-     
+void main(void)
+{
+  char input[100];
+  FILE*fp;
+  int i;
+  
+  fp = fopen("sample.txt","w");
+  
+  for(i=0;i<3;i++)
+  {
+  //fprintf
+  printf("input a word:");
+  scanf("%s", input);
+  fprintf(fp,"%s\n",input);
+}
+  
+  //fclose
+  fclose(fp);
+       
   system("PAUSE");	
   return 0;
 }
